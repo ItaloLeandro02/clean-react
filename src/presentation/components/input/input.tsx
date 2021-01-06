a1bda66a856def7a8f36b51629c81db719b8678b
Italo Leandro <leandroitalo433@gmail.com>
2021-01-06 00:47:41 -0300

    feat: ensure Login start with inputs with required state

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Styles from './input-styles.scss'
 import Dot from '@/presentation/components/dot/dot'
+import Context from '@/presentation/contexts/form/form-context'
 
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
+  const { errorState } = useContext(Context)
+  const error = errorState[props.name]
   const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
     event.target.readOnly = false
+  }
+  const getStatus = (): string => {
+    return 'danger'
+  }
+
+  const getTitle = (): string => {
+    return error
   }
 
   return (
     <div className={Styles.inputWrap}>
       <input {...props} readOnly onFocus={enableInput}/>
-      <span className={Styles.status}>
-        <Dot className="danger"/>
+      <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>
+        <Dot data-testid={`dot-${props.name}-status`} className={getStatus()}/>
       </span>
     </div>
   )
