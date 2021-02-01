42590b32515b155141f5d6b551c8d884c628dcce
Italo Leandro <leandroitalo433@gmail.com>
2021-01-31 23:45:03 -0300

    feat: ensure Login present valid state if password is valid

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -20,11 +20,11 @@ const Input: React.FC<Props> = (props: Props) => {
   }
 
   const getStatus = (): string => {
-    return 'danger'
+    return error ? 'danger' : 'success'
   }
 
   const getTitle = (): string => {
-    return error
+    return error || 'Tudo certo!'
   }
 
   return (
