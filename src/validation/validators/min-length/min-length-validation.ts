bdb13a951fb24fc10ee6c10aca10df2672128af6
Italo Leandro <leandroitalo433@gmail.com>
2021-03-03 00:30:20 -0300

    feat: ensure MinLengthValidation retruns error if value is invalid

---
+++ src/validation/validators/min-length/min-length-validation.ts
@@ -0,0 +1,13 @@
+import { InvalidFieldError } from '@/validation/errors'
+import { FieldValidation } from '@/validation/protocols'
+
+export class MinLengthValidation implements FieldValidation {
+  constructor (readonly field: string, private readonly minLength: number) {}
+
+  fieldName: string
+
+  validate (value: string): Error {
+    this.fieldName = value
+    return new InvalidFieldError()
+  }
+}
