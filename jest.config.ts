6a6757d355d8d47bc3d3c4ff0c505bc29f6b43ab
Italo Leandro <italo_leandro@outlook.com>
2023-08-08 01:56:14 -0300

    chore: config jest

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import { pathsToModuleNameMapper } from 'ts-jest'
+import { compilerOptions } from './tsconfig.json'
+import type { JestConfigWithTsJest } from 'ts-jest'
+
+const jestConfig: JestConfigWithTsJest = {
+  preset: 'ts-jest',
+  roots: [
+    '<rootDir>/tests'
+  ],
+  collectCoverageFrom: [
+    '<rootDir>/tests/**/*.{ts,tsx}'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+}
+
+export default jestConfig
