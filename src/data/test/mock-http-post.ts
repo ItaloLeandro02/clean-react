ad742ff1b15ddcce548a823005bf6f27d48918c4
Italo Leandro <italo_leandro@outlook.com>
2023-08-20 22:40:42 -0300

    refactor: move axios mock to a separete file

---
+++ src/data/test/mock-http-post.ts
@@ -0,0 +1,9 @@
+import { faker } from '@faker-js/faker'
+import { type HttpPostParams } from '@/data/protocols/http'
+
+export const mockPostRequest = (): HttpPostParams<any> => ({
+  url: faker.internet.url(),
+  body: {
+    name: faker.person.fullName()
+  }
+})
