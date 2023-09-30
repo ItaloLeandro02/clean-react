b72ce4b3d65ca4d594cae5fda7fc0bf28553e274
Italo Leandro <italo_leandro@outlook.com>
2023-09-30 00:42:49 -0300

    test: ensure RegexEmailValidator returns falsy if email is invalid

---
+++ tests/infra/email-validator/regex-email-validator/regex-email-validator.spec.ts
@@ -0,0 +1,12 @@
+import { faker } from '@faker-js/faker'
+import { RegexEmailValidator } from '@/infra/email-validator/regex-email-validator/regex-email-validator'
+
+const makeSut = (): RegexEmailValidator => new RegexEmailValidator()
+
+describe('RegexEmailValidator', () => {
+  test('Should return falsy if email is invalid', () => {
+    const sut = makeSut()
+    const result = sut.validate(faker.word.adjective())
+    expect(result).toBeFalsy()
+  })
+})
