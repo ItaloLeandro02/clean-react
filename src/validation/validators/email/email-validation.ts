b7004fb382ab6d01909e23e5b5cf6ec82f9690b0
Italo Leandro <italo_leandro@outlook.com>
2023-09-30 00:34:15 -0300

    feat: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -1,10 +1,15 @@
 import { InvalidFieldValidation } from '@/validation/errors'
-import type { FieldValidation } from '@/validation/protocols'
+import type { EmailValidator, FieldValidation } from '@/validation/protocols'
 
 export class EmailValidation implements FieldValidation {
-  constructor (readonly field: string) {}
+  constructor (
+    readonly field: string,
+    readonly emailValidator: EmailValidator
+  ) {}
 
   validate (value: string): Error {
-    return new InvalidFieldValidation(this.field)
+    return this.emailValidator.validate(value)
+      ? null
+      : new InvalidFieldValidation(this.field)
   }
 }
