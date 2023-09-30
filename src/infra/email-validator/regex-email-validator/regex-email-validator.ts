06bf65fff297f4aca9e9df468d88c48691e4853b
Italo Leandro <italo_leandro@outlook.com>
2023-09-30 00:52:12 -0300

    feat: ensure RegexEmailValidator returns truthy if email is valid

--- src/infra/email-validator/regex-email-validator/regex-email-validator.ts
+++ src/infra/email-validator/regex-email-validator/regex-email-validator.ts
@@ -2,6 +2,7 @@ import type { EmailValidator } from '@/validation/protocols'
 
 export class RegexEmailValidator implements EmailValidator {
   validate (email: string): boolean {
-    return false
+    const emailRegex = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i
+    return emailRegex.test(email)
   }
 }
