5c0f93b580262b5426b1646d61838b328e03047d
Italo Leandro <italo_leandro@outlook.com>
2023-10-01 17:10:20 -0300

    feat: ensure ValidationComposite returns falsy if there is no error

--- src/validation/validators/validation-composite/validation-composite.ts
+++ src/validation/validators/validation-composite/validation-composite.ts
@@ -1,7 +1,16 @@
 import type { Validation } from '@/presentation/protocols/validation'
+import type { FieldValidation } from '@/validation/protocols'
 
 export class CompositeValidation implements Validation {
+  constructor (private readonly fieldValidators: FieldValidation[]) {}
+
   validate (fieldName: string, fieldValue: string): string {
-    return 'First error message'
+    const validators = this.fieldValidators.filter(({ field }) => field === fieldName)
+    for (const validator of validators) {
+      const error = validator.validate(fieldValue)
+      if (error) {
+        return error.message
+      }
+    }
   }
 }
