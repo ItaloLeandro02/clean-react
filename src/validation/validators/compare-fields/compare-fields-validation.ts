9773aa8e864f562e8b0d6abbb7c71e866d9e8806
Italo Leandro <italo_leandro@outlook.com>
2023-11-03 19:06:00 -0300

    feat: ensure CompareFieldsValidation returns error if validation fails

---
+++ src/validation/validators/compare-fields/compare-fields-validation.ts
@@ -0,0 +1,13 @@
+import { InvalidFieldValidation } from '@/validation/errors'
+import { type FieldValidation } from '@/validation/protocols'
+
+export class CompareFieldsValidation implements FieldValidation {
+  constructor (
+    readonly field: string,
+    private readonly valueToCompare: string
+  ) {}
+
+  validate (value: string): Error {
+    return new InvalidFieldValidation(this.field)
+  }
+}
