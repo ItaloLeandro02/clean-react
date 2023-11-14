ad5d6d29bbbf8f4d30f4d269071b2cae6adfb6fc
Italo Leandro <italo_leandro@outlook.com>
2023-11-14 01:25:22 -0300

    test: ensure SignUp enable submit button if form is valid

--- tests/presentation/pages/signup/signup.spec.tsx
+++ tests/presentation/pages/signup/signup.spec.tsx
@@ -92,4 +92,11 @@ describe('SignUp Component', () => {
     Helper.populateField(sut, 'passwordConfirmation')
     Helper.testStatusForField(sut, 'passwordConfirmation')
   })
+
+  test('Should enable submit button if form is valid', () => {
+    const { sut } = makeSut()
+    Helper.populateField(sut, 'email')
+    Helper.populateField(sut, 'password')
+    Helper.testButtonIsDisabled(sut, 'submit', false)
+  })
 })
