10e806e474de6816a5b848b8b089b86fe23419ee
Italo Leandro <italo_leandro@outlook.com>
2023-11-14 00:27:44 -0300

    test: ensure SignUp change name status to error if validation fails

--- tests/presentation/pages/login/login.spec.tsx
+++ tests/presentation/pages/login/login.spec.tsx
@@ -22,9 +22,9 @@ type SutParams = {
 
 const makeSut = (params?: SutParams): SutTypes => {
   const validationStub = new ValidationStub()
+  validationStub.errorMessage = params?.validationError
   const authenticationSpy = new AuthenticationSpy()
   const saveAccessTokenMock = new SaveAccessTokenMock()
-  validationStub.errorMessage = params?.validationError
   const routes: RouteObject[] = [
     {
       path: '/',
