d2769c336eb721e1491b4e536ec129b63cf7e6e4
Italo Leandro <italo_leandro@outlook.com>
2023-11-13 23:24:57 -0300

    feat: add signup view

--- src/presentation/router/router.tsx
+++ src/presentation/router/router.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { makeLogin } from '@/main/factories/pages/login'
+import { SignUp } from '@/presentation/pages'
 
 const router = createBrowserRouter([
   {
     path: '/login',
     Component: makeLogin
+  },
+  {
+    path: '/signup',
+    Component: SignUp
   }
 ])
 
