25e38648a8ba772b883f18e6cf6105158db7947e
Italo Leandro <italo_leandro@outlook.com>
2023-11-24 00:09:51 -0300

    test: ensure Login present error state if form is invalid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker'
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login')
@@ -9,6 +11,21 @@ describe('Login', () => {
       .should('have.text', '🔴')
     cy.getByTestId('password-status')
       .should('have.attr', 'title', 'Campo obrigatório')
+      .should('have.text', '🔴')
+    cy.getByTestId('submit')
+      .should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap')
+      .should('not.have.descendants')
+  })
+
+  it('Should present error state if form is invalid', () => {
+    cy.getByTestId('email').type(faker.lorem.word())
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Campo email inválido!')
+      .should('have.text', '🔴')
+    cy.getByTestId('password').type(faker.lorem.word(4))
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Campo password inválido!')
       .should('have.text', '🔴')
     cy.getByTestId('submit')
       .should('have.attr', 'disabled')
