c5a17b98cfa641af8646b63ed0f92ba1dfc8b83d
Italo Leandro <italo_leandro@outlook.com>
2023-12-05 23:32:50 -0300

    test: ensure AxiosHttpClient calls axios.get with correct values

--- src/data/test/mock-http.ts
+++ src/data/test/mock-http.ts
@@ -22,6 +22,10 @@ export const mockPostRequest = (): HttpPostParams => ({
   }
 })
 
+export const mockGettRequest = (): HttpGetParams => ({
+  url: faker.internet.url()
+})
+
 export class HttpGetClientSpy<R> implements HttpGetClient<R> {
   url: string
   response: HttpResponse<R> = {
