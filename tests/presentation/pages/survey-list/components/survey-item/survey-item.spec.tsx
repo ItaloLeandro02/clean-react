64b48ed726497e2ec53a00254deac516648b4ae4
Italo Leandro <italo_leandro@outlook.com>
2023-12-17 17:42:25 -0300

    test: ensure SurveyItem load icon and day correctly

--- tests/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
+++ tests/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
@@ -3,17 +3,36 @@ import { render, screen } from '@testing-library/react'
 import { SurveyItem } from '@/presentation/pages/survey-list/components'
 import { IconName } from '@/presentation/components'
 import { mockSurveyModel } from '@/domain/test'
+import { type SurveyModel } from '@/domain/models'
+
+const makeSut = (survey: SurveyModel): void => {
+  render(<SurveyItem survey={survey} />)
+}
 
 describe('SurveyItem Component', () => {
   test('Should render with correct values', () => {
-    const survey = mockSurveyModel()
-    survey.didAnswer = true
-    survey.date = new Date('2022-12-17T00:00:00')
-    render(<SurveyItem survey={survey} />)
+    const survey = Object.assign(mockSurveyModel(), {
+      didAnswer: true,
+      date: new Date('2022-12-17T00:00:00')
+    })
+    makeSut(survey)
     expect(screen.getByTestId('icon')).toHaveAttribute('src', IconName.thumbUp)
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
     expect(screen.getByTestId('day')).toHaveTextContent('17')
     expect(screen.getByTestId('month')).toHaveTextContent('dez')
     expect(screen.getByTestId('year')).toHaveTextContent('2022')
+  })
+
+  test('Should render with correct values', () => {
+    const survey = Object.assign(mockSurveyModel(), {
+      didAnswer: false,
+      date: new Date('2021-11-03T00:00:00')
+    })
+    makeSut(survey)
+    expect(screen.getByTestId('icon')).toHaveAttribute('src', IconName.thumbDown)
+    expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
+    expect(screen.getByTestId('day')).toHaveTextContent('03')
+    expect(screen.getByTestId('month')).toHaveTextContent('nov')
+    expect(screen.getByTestId('year')).toHaveTextContent('2021')
   })
 })
