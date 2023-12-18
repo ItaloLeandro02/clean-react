fae0285e1635918403e94c6872b85ddc3f7c8f61
Italo Leandro <italo_leandro@outlook.com>
2023-12-18 00:15:40 -0300

    refactor: move LoadSurveyListSpy to its own file

--- tests/presentation/pages/survey-list/survey-list.spec.tsx
+++ tests/presentation/pages/survey-list/survey-list.spec.tsx
@@ -1,20 +1,8 @@
 import React from 'react'
 import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import { SurveyList } from '@/presentation/pages'
-import { type LoadSurveyList } from '@/domain/usecases'
-import { type SurveyModel } from '@/domain/models'
 import { UnexpectedError } from '@/domain/errors'
-import { mockSurveyList } from '@/domain/test'
-
-class LoadSurveyListSpy implements LoadSurveyList {
-  callsCount = 0
-  surveys = mockSurveyList()
-
-  async loadAll (): Promise<SurveyModel[]> {
-    this.callsCount++
-    return this.surveys
-  }
-}
+import { LoadSurveyListSpy } from '@/presentation/test'
 
 type SutTypes = {
   loadSurveyListSpy: LoadSurveyListSpy
