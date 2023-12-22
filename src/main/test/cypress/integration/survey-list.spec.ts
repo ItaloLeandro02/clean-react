8c0cfd01859c39d6b24a5842cf9351050df8b97d
Italo Leandro <italo_leandro@outlook.com>
2023-12-22 02:08:56 -0300

    test: ensure SurveyList presents error on UnexpectedError

---
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -0,0 +1,15 @@
+import { faker } from '@faker-js/faker'
+import * as Helper from '../support/helper'
+import * as Http from '../support/survey-list-mock'
+
+describe('SurveyList', () => {
+  beforeEach(() => {
+    Helper.setLocalStorageItem('account', { accessToken: faker.string.uuid(), name: faker.person.firstName() })
+  })
+
+  it('Should present error on UnexpectedError', () => {
+    Http.mockUnexpectedError()
+    cy.visit('')
+    cy.getByTestId('error').should('contain.text', 'Something went wrong. Try again')
+  })
+})
