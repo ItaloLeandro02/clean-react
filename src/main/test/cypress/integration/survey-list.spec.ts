08826be67cbe4c61a040247bfa50be700fea71fa
Italo Leandro <italo_leandro@outlook.com>
2023-12-23 00:49:04 -0300

    test: ensure SurveyList presents surveys on success

--- src/main/test/cypress/integration/survey-list.spec.ts
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -4,6 +4,11 @@ import * as Http from '../utils/http-mocks'
 const path = 'api/surveys'
 const mockUnexpectedError = (): void => { Http.mockServerError('GET', path) }
 const mockAccessDeniedError = (): void => { Http.mockForbiddenError('GET', path) }
+const mockSuccess = (): void => {
+  cy.fixture('survey-list').then((surveys) => {
+    Http.mockOk('GET', path, surveys)
+  })
+}
 
 describe('SurveyList', () => {
   beforeEach(() => {
@@ -36,5 +41,30 @@ describe('SurveyList', () => {
     cy.visit('')
     cy.getByTestId('logout').click()
     Helper.testUrl('/login')
+  })
+
+  it('Should present survey items', () => {
+    mockSuccess()
+    cy.visit('')
+    cy.get('li:empty').should('have.length', 4)
+    cy.get('li:not(empty)').should('have.length', 2)
+    cy.get('li:nth-child(1)').then((li) => {
+      assert.equal(li.find('[data-testid=day]').text(), '02')
+      assert.equal(li.find('[data-testid=month]').text(), 'nov')
+      assert.equal(li.find('[data-testid=year]').text(), '2020')
+      assert.equal(li.find('[data-testid=question]').text(), 'Question 1')
+      cy.fixture('icons').then((icons) => {
+        assert.equal(li.find('[data-testid=icon]').attr('src'), icons.thumbUp)
+      })
+    })
+    cy.get('li:nth-child(2)').then((li) => {
+      assert.equal(li.find('[data-testid=day]').text(), '16')
+      assert.equal(li.find('[data-testid=month]').text(), 'mai')
+      assert.equal(li.find('[data-testid=year]').text(), '2022')
+      assert.equal(li.find('[data-testid=question]').text(), 'Question 2')
+      cy.fixture('icons').then((icons) => {
+        assert.equal(li.find('[data-testid=icon]').attr('src'), icons.thumbDown)
+      })
+    })
   })
 })
