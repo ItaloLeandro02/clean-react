49ad784378d52e9d04721333ff9b2c97ea27809a
Italo Leandro <italo_leandro@outlook.com>
2023-12-24 13:59:15 -0300

    fix: chanfe compare fields validation test

--- tests/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ tests/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -6,8 +6,8 @@ const makeSut = (field: string, fieldToCompare: string): CompareFieldsValidation
 
 describe('CompareFieldsValidation', () => {
   test('Should return error if compare is invalid', () => {
-    const field = faker.database.column()
-    const fieldToCompare = faker.database.column()
+    const field = 'any_field'
+    const fieldToCompare = 'another_any_field'
     const sut = makeSut(field, fieldToCompare)
     const error = sut.validate({
       [field]: 'any_value',
