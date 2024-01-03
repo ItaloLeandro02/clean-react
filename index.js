6e6603137aedf8efc048121eb2aaee8e919e0c6c
Italo Leandro <italo_leandro@outlook.com>
2024-01-03 00:31:55 -0300

    chore: config actions to cypress

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const app = express()
 const root = `${__dirname}/dist`
 app.use(express.static(root))
 app.use(fallback('index.html', { root }))
-app.listen(process.env.PORT || 3000)
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Server is running on ${port}`))
