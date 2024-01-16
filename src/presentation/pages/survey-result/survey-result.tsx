d5ecb7f030f13b66b19a7267d396818a0dd456f1
Italo Leandro <italo_leandro@outlook.com>
2024-01-16 00:36:15 -0300

    feat: add skeleton with shimmer to SurveyResult

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -8,29 +8,42 @@ const SurveyResult: React.FC = () => {
     <div className={Styles.surveyResultWrap}>
       <Header />
       <div className={Styles.contentWrap}>
-        <hgroup>
-          <Calendar date={new Date()} className={Styles.calendarWrap} />
-          <h2>Qual é o seu framework web favorito</h2>
-        </hgroup>
-        <FlipMove className={Styles.answersList}>
-          <li>
-            <img src="https://www.logo.wine/a/logo/React_(web_framework)/React_(web_framework)-Logo.wine.svg" alt="" />
-            <span className={Styles.answer}>ReactJs</span>
-            <span className={Styles.percent}>50%</span>
-          </li>
-          <li className={Styles.active}>
-            <img src="https://www.logo.wine/a/logo/React_(web_framework)/React_(web_framework)-Logo.wine.svg" alt="" />
-            <span className={Styles.answer}>ReactJs</span>
-            <span className={Styles.percent}>50%</span>
-          </li>
-          <li>
-            <img src="https://www.logo.wine/a/logo/React_(web_framework)/React_(web_framework)-Logo.wine.svg" alt="" />
-            <span className={Styles.answer}>ReactJs</span>
-            <span className={Styles.percent}>50%</span>
-          </li>
-        </FlipMove>
-        <button>Voltar</button>
-        { false && <Loading /> }
+        {true && (
+          <>
+            <hgroup>
+              <Calendar date={new Date()} className={Styles.calendarWrap} />
+              <h2>Qual é o seu framework web favorito? Qual é o seu framework web favorito? Qual é o seu framework web favorito?</h2>
+            </hgroup>
+            <FlipMove className={Styles.answersList}>
+              <li>
+                <img
+                  src="https://www.logo.wine/a/logo/React_(web_framework)/React_(web_framework)-Logo.wine.svg"
+                  alt=""
+                />
+                <span className={Styles.answer}>ReactJs</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
+              <li className={Styles.active}>
+                <img
+                  src="https://www.logo.wine/a/logo/React_(web_framework)/React_(web_framework)-Logo.wine.svg"
+                  alt=""
+                />
+                <span className={Styles.answer}>ReactJs</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
+              <li>
+                <img
+                  src="https://www.logo.wine/a/logo/React_(web_framework)/React_(web_framework)-Logo.wine.svg"
+                  alt=""
+                />
+                <span className={Styles.answer}>ReactJs</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
+            </FlipMove>
+            <button>Voltar</button>
+          </>
+        )}
+        {false && <Loading />}
       </div>
       <Footer />
     </div>
