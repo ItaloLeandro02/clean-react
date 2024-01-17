efd5ff94cf8c5a2fd79a7bd2aaf53804a0cb9b6d
Italo Leandro <italo_leandro@outlook.com>
2024-01-16 23:49:03 -0300

    refactor: remove unnecessary Context

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Styles from './survey-list-styles.scss'
 import { Error, Footer, Header } from '@/presentation/components'
-import { SurveyContext, SurveyListItem } from '@/presentation/pages/survey-list/components'
+import { SurveyListItem } from '@/presentation/pages/survey-list/components'
 import { useErrorHandler } from '@/presentation/components/hooks'
 import { LoadSurveyList } from '@/domain/usecases'
 
@@ -31,9 +31,7 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
       <Header />
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
-        <SurveyContext.Provider value={{ state, setState }}>
-          {state.error ? <Error error={state.error} reload={reload} /> : <SurveyListItem />}
-        </SurveyContext.Provider>
+          {state.error ? <Error error={state.error} reload={reload} /> : <SurveyListItem surveys={state.surveys} />}
       </div>
       <Footer />
     </div>
