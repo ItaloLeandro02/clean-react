10cc70632e04c0db989193f65fe724e1d6c69ac5
Italo Leandro <italo_leandro@outlook.com>
2024-01-18 02:24:50 -0300

    feat: ensure SurveyResult presents correct result on success

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -1,7 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import FlipMove from 'react-flip-move'
 import Styles from './survey-result-styles.scss'
-import { Calendar, Error, Footer, Header, Loading } from '@/presentation/components'
+import {
+  Calendar,
+  Error,
+  Footer,
+  Header,
+  Loading
+} from '@/presentation/components'
 import { LoadSurveyResult } from '@/domain/usecases'
 
 type Props = {
@@ -9,15 +15,18 @@ type Props = {
 }
 
 const SurveyResult: React.FC<Props> = ({ loadSurveyResult }: Props) => {
-  const [state] = useState({
+  const [state, setState] = useState({
     isLoading: false,
     error: '',
     surveyResult: null as LoadSurveyResult.Model
   })
 
   useEffect(() => {
-    loadSurveyResult.load()
-      .then()
+    loadSurveyResult
+      .load()
+      .then((surveyResult) => {
+        setState({ ...state, surveyResult })
+      })
       .catch()
   }, [])
 
@@ -28,34 +37,26 @@ const SurveyResult: React.FC<Props> = ({ loadSurveyResult }: Props) => {
         {state.surveyResult && (
           <>
             <hgroup>
-              <Calendar date={new Date()} className={Styles.calendarWrap} />
-              <h2>Qual é o seu framework web favorito? Qual é o seu framework web favorito? Qual é o seu framework web favorito?</h2>
+              <Calendar
+                date={state.surveyResult.date}
+                className={Styles.calendarWrap}
+              />
+              <h2 data-testid="question">{state.surveyResult.question}</h2>
             </hgroup>
-            <FlipMove className={Styles.answersList}>
-              <li>
-                <img
-                  src="https://www.logo.wine/a/logo/React_(web_framework)/React_(web_framework)-Logo.wine.svg"
-                  alt=""
-                />
-                <span className={Styles.answer}>ReactJs</span>
-                <span className={Styles.percent}>50%</span>
-              </li>
-              <li className={Styles.active}>
-                <img
-                  src="https://www.logo.wine/a/logo/React_(web_framework)/React_(web_framework)-Logo.wine.svg"
-                  alt=""
-                />
-                <span className={Styles.answer}>ReactJs</span>
-                <span className={Styles.percent}>50%</span>
-              </li>
-              <li>
-                <img
-                  src="https://www.logo.wine/a/logo/React_(web_framework)/React_(web_framework)-Logo.wine.svg"
-                  alt=""
-                />
-                <span className={Styles.answer}>ReactJs</span>
-                <span className={Styles.percent}>50%</span>
-              </li>
+            <FlipMove data-testid="answers" className={Styles.answersList}>
+              {state.surveyResult.answers.map((answer) => (
+                <li data-testid="answer-wrap" key={answer.answer} className={answer.isCurrentAccountAnswer ? Styles.active : ''}>
+                  {answer.image && (
+                    <img
+                      data-testid="image"
+                      src={answer.image}
+                      alt={answer.answer}
+                    />
+                  )}
+                  <span data-testid="answer" className={Styles.answer}>{answer.answer}</span>
+                  <span data-testid="percent" className={Styles.percent}>{answer.percent}%</span>
+                </li>
+              ))}
             </FlipMove>
             <button>Voltar</button>
           </>
