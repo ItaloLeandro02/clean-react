18f714fc83324441cac59f40f99c7b39b9ba3940
Italo Leandro <italo_leandro@outlook.com>
2024-01-29 23:28:58 -0300

    refactor: fixing typos

--- src/domain/usecases/save-survey-result.ts
+++ src/domain/usecases/save-survey-result.ts
@@ -1,4 +1,4 @@
-import { SurveyResultModel, SurveyResultAnswerModel } from '@/domain/models'
+import { SurveyResultModel } from '@/domain/models'
 
 export interface SaveSurveyResult {
   save: (params: SaveSurveyResult.Params) => Promise<SaveSurveyResult.Model>
@@ -6,7 +6,7 @@ export interface SaveSurveyResult {
 
 export namespace SaveSurveyResult {
   export type Params = {
-    answer: SurveyResultAnswerModel
+    answer: string
   }
 
   export type Model = SurveyResultModel
