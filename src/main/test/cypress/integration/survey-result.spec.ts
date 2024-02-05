967a9c28e66a5d9d3fd6727088a4c746703acd7c
Italo Leandro <italo_leandro@outlook.com>
2024-02-04 23:48:42 -0300

    test: ensure SurveyResult logout on AccessDeniedError

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -70,6 +70,7 @@ describe('SurveyResult', () => {
 
   describe('save', () => {
     const mockUnexpectedError = (): void => { Http.mockServerError('PUT', path) }
+    const mockAccessDeniedError = (): void => { Http.mockForbiddenError('PUT', path) }
 
     beforeEach(() => {
       cy.fixture('account').then((account) => {
@@ -83,6 +84,12 @@ describe('SurveyResult', () => {
       mockUnexpectedError()
       cy.get('li:nth-child(2)').click()
       cy.getByTestId('error').should('contain.text', 'Something went wrong. Try again')
+    })
+
+    it('Should logout on AccessDeniedError', () => {
+      mockAccessDeniedError()
+      cy.get('li:nth-child(2)').click()
+      Helper.testUrl('/login')
     })
   })
 })
